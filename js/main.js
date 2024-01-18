3e039b182e11030cc046af3145cb1878dafc0c40
Den Dome <newlifeofkirim@gmail.com>
2024-01-19 06:34:54 +0700

    dopolnitelnaya 2

--- js/main.js
+++ js/main.js
@@ -18,19 +18,20 @@ let app = new Vue({
         inputOne: null,
         inputTwo: null,
         inputThr: null,
+        inputFor: null,
+        inputFiv: null,
+        important: null
     },
     watch: {
         firstColumn: {
             handler(newFirstColumn) {
                 this.saveData();
-                this.checkBlockColumn();
             },
             deep: true
         },
         secondColumn: {
             handler(newSecondColumn) {
                 this.saveData();
-                this.checkBlockColumn();
             },
             deep: true
         },
@@ -41,7 +42,34 @@ let app = new Vue({
             deep: true
         }
     },
+    computed: {
+        isDisabled() {
+            return function (groupIndex, item) {
+
+                return item.checked || this.isGroupLastItemDisabled[groupIndex] === item;
+            };
+        },
+        isGroupLastItemDisabled() {
+            return this.firstColumn.map(group => {
+                if (this.secondColumn.length >= 5 && group.items.length > 0) {
+                    const lastUncheckedItem = group.items.slice().reverse().find(item => !item.checked);
+                    return lastUncheckedItem;
+                }
+
+                return null;
+            });
+        },
+    },
     methods: {
+        deleteCard(column, cardIndex) {
+            if (confirm("Are you sure you want to delete this card?")) {
+                if (column === 'first') {
+                    this.firstColumn.splice(cardIndex, 1);
+                } else if (column === 'second') {
+                    this.secondColumn.splice(cardIndex, 1);
+                }
+            }
+        },
         saveData() {
             const data = {
                 firstColumn: this.firstColumn,
@@ -59,30 +87,23 @@ let app = new Vue({
             }
             this.checkMoveCard();
         },
-        checkBlockColumn() {
-            const inProgressCount = this.firstColumn.filter(card => {
-                const progress = (card.items.filter(item => item.checked).length / card.items.length) * 100;
-                return progress > 50;
-            }).length;
-
-            const isMaxSecondColumn = this.secondColumn.length >= 5;
-
-            if (inProgressCount > 0 && isMaxSecondColumn) {
-                this.firstColumn.forEach(card => {
-                    card.items.forEach(item => {
-                        item.checked = false;
-                    });
-                });
-            }
-        },
-        checkMoveCard() {
+        MoveFirstColm() {
             this.firstColumn.forEach(card => {
                 const progress = (card.items.filter(item => item.checked).length / card.items.length) * 100;
-                if (progress >= 50) {
+
+                const isMaxSecondColumn = this.secondColumn.length >= 5;
+
+                if (progress >= 50 && !isMaxSecondColumn) {
                     this.secondColumn.push(card);
                     this.firstColumn.splice(this.firstColumn.indexOf(card), 1);
+                    this.MoveSecondColm();
+                }
+                else {
                 }
             });
+
+        },
+        MoveSecondColm() {
             this.secondColumn.forEach(card => {
                 const progress = (card.items.filter(item => item.checked).length / card.items.length) * 100;
                 if (progress === 100) {
@@ -90,27 +111,44 @@ let app = new Vue({
                     card.lastChecked = new Date().toLocaleString();
                     this.thirdColumn.push(card);
                     this.secondColumn.splice(this.secondColumn.indexOf(card), 1);
+                    this.MoveFirstColm();
                 }
             })
         },
+        checkMoveCard() {
+            this.MoveFirstColm();
+            this.MoveSecondColm();
+            this.sortedColumns();
+        },
         addCard() {
-            const newGroup = {
-                id: Date.now(),
-                groupName: this.groupName,
-                items: [
-                    { text: this.inputOne, checked: false },
-                    { text: this.inputTwo, checked: false },
-                    { text: this.inputThr, checked: false },
-                ]
+            const inputs = [this.inputOne, this.inputTwo, this.inputThr, this.inputFor, this.inputFiv];
+            const validInputs = inputs.filter(input => input !== null && input.trim() !== '');
+            const numItems = Math.max(3, Math.min(5, validInputs.length));
+
+            if (this.groupName && this.important) {
+                const newGroup = {
+                    id: Date.now(),
+                    groupName: this.groupName,
+                    items: validInputs.slice(0, numItems).map(text => ({ text, checked: false })),
+                    important: this.important,
+                }
+                if (this.firstColumn.length < 3) {
+                    this.firstColumn.push(newGroup)
+                    this.sortedColumns()
+                }
             }
-            this.firstColumn.push(newGroup),
-                this.groupName = null,
-                this.inputOne = null,
-                this.inputTwo = null,
-                this.inputThr = null
+            this.groupName = null,
+            this.inputOne = null,
+            this.inputTwo = null,
+            this.inputThr = null,
+            this.inputFor = null,
+            this.inputFiv = null,
+            this.important = null
+        },
+        sortedColumns(){
+            this.firstColumn.sort((a, b) => b.important - a.important);  
+            this.secondColumn.sort((a, b) => b.important - a.important); 
+            this.thirdColumn.sort((a, b) => b.important - a.important); 
         }
     },
-    mounted() {
-        this.checkBlockColumn();
-    }
 })
